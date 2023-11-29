55574e0209a0ea51acc37131b90602f8fce2ed49
Uliana Boiko <clubfate@gmail.com>
2023-11-30 00:52:15 +0200

    Start

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const doc = document;
+
+const display = doc.querySelector('.display');
+
+const formTag = doc.forms.formTag;
+const tag = formTag.tag;
+const selectBtnTag = formTag.selectBtnTag;
+
+const formTagCreate = doc.forms.formTagCreate;
+const addNewTag = formTagCreate.addNewTag;
+const titleRedWarn = doc.getElementById('red');
+const addTagBtn = formTagCreate.addTagBtn;
+const start = doc.querySelector('.start');
+
+const formInfo = doc.forms.formInfo;
+const addText = formInfo.addText;
+const addClass = formInfo.addClass;
+const addLink = formInfo.addLink;
+const addSrc = formInfo.addSrc;
+const addTextBtn = formInfo.addTextBtn;
+const addClassBtn = formInfo.addClassBtn;
+const addLinkBtn = formInfo.addLinkBtn;
+const addSrcBtn = formInfo.addSrcBtn;
+const errorText = doc.querySelector('.note');
+const errorLink = doc.querySelector('.error-link');
+const errorSrc = doc.querySelector('.error-src');
+
+const formStyle = doc.forms.formStyle;
+const addStyle = formStyle.addStyle;
+const addStyleBtn = formStyle.addStyleBtn;
+
+
+
+
+
+console.log();
+
+
+
